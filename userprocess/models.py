4e44088753d581c42bf179a85b4362b87faf7fae
lukas <lukas.le@gmx.at>
2019-05-04 17:25:44 +0200

    dkdkddkf i

---
+++ userprocess/models.py
@@ -0,0 +1,6 @@
+# from django.db import models
+#
+# class Userpath()
+#
+# class Userupload(models.Model):
+#     herzunter = models.ImageField(upload_to)
---
+++ userprocess/views.py
@@ -0,0 +1,32 @@
+import os
+import random
+import string
+from django.shortcuts import render, redirect, get_object_or_404
+from django.views.decorators.csrf import csrf_exempt
+from django.http import HttpResponse
+from userprocess import faceswap
+from watten.settings import MEDIA_ROOT
+import pybase64
+
+def home(request):
+    return render(request, 'home.html')
+
+def randomString(stringLength=10):
+    """Generate a random string of fixed length """
+    letters = string.ascii_lowercase
+    return ''.join(random.choice(letters) for i in range(stringLength))
+
+@csrf_exempt
+def process_image(request):
+    if 'imgdata' in request.POST:
+        imgdata = request.POST['imgdata']
+        header, encoded = imgdata.split(",", 1)
+        imgdata_stripped = pybase64.b64decode(encoded)
+        userdirectory = randomString()
+        userpath = os.path.join(MEDIA_ROOT, userdirectory)
+        os.mkdir(userpath)
+        with open(os.path.join(userpath, 'image.jpg'), "wb") as f:
+            f.write(imgdata_stripped)
+        faceswap.main(userdirectory)
+        return render(request, 'home.html', {'userpath': userpath})
+        #return HttpResponse("$('#convertedimage').prop('src', 'media/' + userdirectory + '/output.jpg?' + new Date().valueOf())", content_type="application/x-javascript")
